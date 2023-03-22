94b3500429e34bfbc17fd82b926f62699ffb21b6
Dennis Minn <minndennis@gmail.com>
2023-03-22 19:55:43 -0400

    added training script

---
+++ train.py
@@ -0,0 +1,33 @@
+import pytorch_lightning as pl
+from data_modules import *
+from data_modules.bAbI import *
+
+from logger import *
+from models import *
+
+if __name__ == "__main__":
+
+    model = QuestionAnswerModel("google/flan-t5-small")
+    logger = QuestionAnswerLogger()
+
+    trainer = pl.Trainer(max_epochs=1, callbacks=[logger])
+
+    for i in range(1, 21, 1):
+        datamodule = bAbIDataModule(
+            model_name="google/flan-t5-small",
+            batch_size=2,
+            num_demonstrations=5,
+            train_path=f"data/bAbI tasks_1-20_v1-2/en-valid/qa{i}_train.txt",
+            validation_path=f"data/bAbI tasks_1-20_v1-2/en-valid/qa{i}_valid.txt",
+            test_path=f"data/bAbI tasks_1-20_v1-2/en-valid/qa{i}_test.txt"
+        )
+
+        datamodule.setup()
+
+        trainer.datamodule = datamodule
+
+        train_dataloader = trainer.datamodule.train_dataloader()
+        validation_dataloader = trainer.datamodule.val_dataloader()
+        test_dataloader = trainer.datamodule.test_dataloader()
+
+        trainer.validate(model=model, dataloaders=[validation_dataloader, validation_dataloader, validation_dataloader])
