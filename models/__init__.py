36c5283b3cb58e6f7bfc8d7fed17ffd154436ade
Dennis Minn <minndennis@gmail.com>
2023-05-20 03:43:14 +0000

    updated model

--- models/__init__.py
+++ models/__init__.py
@@ -8,19 +8,58 @@ if TYPE_CHECKING:
     from torch import BatchEncoding
     from data_module.bAbI import bAbIItem
 
-IGNORE_TOKEN = -100
+NER_MODEL_PATH = "dslim/bert-base-NER"
+PERSON_ENTITY = "PER"
+SUBWORD_TOKEN = "##"
+CONFIDENCE_THRESHOLD = 0.5
+
+
+class NERModel():
+    def __init__(self):
+        from transformers import AutoModelForTokenClassification, AutoTokenizer
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = AutoModelForTokenClassification.from_pretrained(NER_MODEL_PATH).to(device)
+        self.tokenizer = AutoTokenizer.from_pretrained(NER_MODEL_PATH)
+
+    def annotate_entities(self, text):
+        from transformers import pipeline
+        device = 0 if torch.cuda.is_available() else -1
+        annotations = pipeline("ner", model=self.model, tokenizer=self.tokenizer, device=device)(text)
+        entities = []
+
+        for i, token in enumerate(annotations):
+            if not token["entity"].endswith(PERSON_ENTITY):
+                continue
+
+            if token["score"] < CONFIDENCE_THRESHOLD:
+                continue
+
+            if SUBWORD_TOKEN not in token["word"]:
+                entities.append(token["word"])
+            elif (
+                SUBWORD_TOKEN in token["word"] and
+                len(entities) and
+                len(annotations) and
+                annotations[i]["start"] == annotations[i-1]["end"]
+            ):
+                entities[-1] += token["word"].replace(SUBWORD_TOKEN, "")
+            else:
+                pass
+
+        return set(entities)
 
 
 class QuestionAnswerModel(LightningModule):
-    def __init__(self, model_name: str, max_new_tokens=128):
+    def __init__(self, data_module, max_new_tokens=64):
         super().__init__()
-        if "flan-t5" in model_name:
-            from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-            self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-        elif "gpt-j" in model_name or "gpt-neo-1.3B" in model_name:
-            from transformers import AutoModelForCausalLM, AutoTokenizer
-            self.model = AutoModelForCausalLM.from_pretrained(model_name)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.tokenizer = data_module.tokenizer
+        self.model_name = self.tokenizer.name_or_path
+        if "flan-t5" in self.model_name:
+            from transformers import AutoModelForSeq2SeqLM
+            self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name)
+        elif "gpt" in self.model_name:
+            from transformers import AutoModelForCausalLM
+            self.model = AutoModelForCausalLM.from_pretrained(self.model_name)
         self.max_new_tokens = max_new_tokens
 
     def forward(self, inputs: "BatchEncoding") -> "torch.Tensor":
@@ -52,7 +91,7 @@ class QuestionAnswerModel(LightningModule):
                 Index of subset
         """
         # generate tokens
-        gen_tokens = self.forward(batch["BatchEncoding"])
+        gen_tokens = self.forward(batch["batch_encoding"])
 
         # decode tokens
         gen_text = self.tokenizer.batch_decode(gen_tokens, skip_special_tokens=True)
@@ -64,7 +103,6 @@ class QuestionAnswerModel(LightningModule):
             item.prediction = gen_text[idx]
 
         prompts_ppl, predictions_ppl, answers_ppl = self.calculate_perplexity(demonstrations, qa_items)
-
         for idx, item in enumerate(qa_items):
             item.prompt_perplexity = prompts_ppl[idx].item()
             item.prediction_perplexity = predictions_ppl[idx].item()
@@ -74,8 +112,12 @@ class QuestionAnswerModel(LightningModule):
 
     def calculate_perplexity(self, demonstrations, qa_items):
         batch_size = len(qa_items)
-        prompts = [item.format(demonstrations, include_answer=False) for item in qa_items]
-        predictions = [item.format(demonstrations, include_answer=False, include_prediction=True) for item in qa_items]
+        prompts_start, prompts_end = 0 * batch_size, 1 * batch_size,
+        predictions_start, predictions_end = 1 * batch_size, 2 * batch_size
+        answers_start, answers_end = 2 * batch_size, 3 * batch_size
+
+        prompts = [item.format(demonstrations) for item in qa_items]
+        predictions = [item.format(demonstrations, include_prediction=True) for item in qa_items]
         answers = [item.format(demonstrations, include_answer=True) for item in qa_items]
 
         batch = prompts + predictions + answers
@@ -85,26 +127,58 @@ class QuestionAnswerModel(LightningModule):
                                          truncation=True,
                                          return_tensors="pt")
 
-        batch_encodings["input_ids"].to(self.device)
-        batch_encodings["attention_mask"].to(self.device)
+        batch_encodings["input_ids"] = batch_encodings["input_ids"].to(self.device)
+        batch_encodings["attention_mask"] = batch_encodings["attention_mask"].to(self.device)
 
         with torch.no_grad():
+            self.model = self.model.to(self.device)
             logits = self.model(**batch_encodings, labels=batch_encodings["input_ids"]).logits
             logits_flatten = logits.reshape(-1, logits.shape[-1])
 
         target_ids = batch_encodings["input_ids"].clone()
+        prediction_ids = target_ids[predictions_start: predictions_end]
+        answer_ids = target_ids[answers_start: answers_end]
+
+        if "flan-t5" in self.model_name:
+            self._mask_target_ids(prompts, prediction_ids)
+            self._mask_target_ids(prompts, answer_ids)
+
+        elif "gpt" in self.model_name:
+            self._mask_target_ids([item.prediction for item in qa_items], prediction_ids, inverse=True)
+            self._mask_target_ids([item.answer for item in qa_items], answer_ids, inverse=True)
+
         target_ids_flatten = target_ids.reshape(-1)
 
-        negative_log_likelihood_loss = F.cross_entropy(logits_flatten,
-                                                       target_ids_flatten,
-                                                       reduction="none",
-                                                       ignore_index=self.tokenizer.pad_token_id)
+        negative_log_likelihood_loss = F.cross_entropy(
+            logits_flatten,
+            target_ids_flatten,
+            reduction="none",
+            ignore_index=self.tokenizer.pad_token_id
+        )
 
-        negative_log_likelihood_loss = negative_log_likelihood_loss.reshape(batch_size * 3, -1)
+        negative_log_likelihood_loss = negative_log_likelihood_loss.reshape(target_ids.shape)
         perplexities = torch.exp(negative_log_likelihood_loss.mean(1))
 
-        prompts_ppl = perplexities[0 * batch_size: 1 * batch_size]
-        predictions_ppl = perplexities[1 * batch_size: 2 * batch_size]
-        answers_ppl = perplexities[2 * batch_size: 3 * batch_size]
+        prompts_ppl = perplexities[prompts_start: prompts_end]
+        predictions_ppl = perplexities[predictions_start: predictions_end]
+        answers_ppl = perplexities[answers_start: answers_end]
 
         return prompts_ppl, predictions_ppl, answers_ppl
+
+    def _mask_target_ids(self, text, target_ids, inverse=False):
+        sequence_length = target_ids.shape[1]
+        ignore_index = self.tokenizer.pad_token_id
+        attention_mask = self.tokenizer(text,
+                                        padding="max_length",
+                                        max_length=sequence_length,
+                                        add_special_tokens=False,
+                                        truncation=True,
+                                        return_tensors="pt")["attention_mask"]
+        attention_mask = attention_mask.to(self.device)
+
+        # TODO target_ids.masked_fill(~attention_mask, ignore_index) doesn't work
+        if not inverse:
+            target_ids.masked_fill_(attention_mask == 1, ignore_index)
+        else:
+            target_ids.masked_fill_(attention_mask == 0, ignore_index)
+        return target_ids
--- train.py
+++ train.py
@@ -1,29 +1,80 @@
+import ast
 import pytorch_lightning as pl
 from models import QuestionAnswerModel
-from data_modules.GSM import GSMDataModule
-from logger.GSM import GSMLogger
+import argparse
 import os
 
-if __name__ == "__main__":
-    model = QuestionAnswerModel("google/flan-t5-base")
 
-    for num_demonstrations in [3, 4, 5]:
-        datamodule = GSMDataModule(
-                model_name="google/flan-t5-base",
-                batch_size=5,
-                demonstration_indices=[1, 4, 70, 88, 94, 106, 126, 132, 149, 196],
-                num_demonstrations=num_demonstrations,
-                data_directory="data/gsm",
-                entities_metadata_fpath="data/entities_metadata.csv",
-                entity_augmentation=None,
-                prompt_augmentation=None,
+if __name__ == "__main__":
+    os.system("wandb offline")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file_name", help="file name")
+    parser.add_argument("--enable-wandb", type=bool, default=True)
+
+    parser.add_argument("--dataset_name", type=str, required=True)
+    parser.add_argument("--model_name", type=str, required=True)
+    parser.add_argument("--batch_size", type=int, required=True)
+    parser.add_argument("--demonstration_indices", type=str)
+    parser.add_argument("--num_demonstrations", type=int, default=-1)
+    parser.add_argument("--data_directory", type=str, required=True)
+    parser.add_argument("--entity_augmentation", type=str)
+    parser.add_argument("--prompt_augmentation", type=str)
+    parser.add_argument("--tasks", type=str)
+
+    parser.add_argument("--max_new_tokens", type=int, required=True)
+
+    args = parser.parse_args()
+
+    callbacks = []
+    if args.dataset_name == "bAbI":
+        from data_modules.bAbI import bAbIDataModule
+        from logger.bAbI import bAbILogger
+
+        args.tasks = ast.literal_eval(args.tasks)
+        args.demonstration_indices = ast.literal_eval(args.demonstration_indices)
+
+        data_module = bAbIDataModule(
+            model_name=args.model_name,
+            batch_size=args.batch_size,
+            data_directory=args.data_directory,
+            tasks=args.tasks,
+            demonstration_indices=args.demonstration_indices,
+            num_demonstrations=args.num_demonstrations,
+            entity_augmentation=args.entity_augmentation,
+            prompt_augmentation=args.prompt_augmentation,
+        )
+        data_module.setup()
+
+        if args.enable_wandb:
+            callbacks.append(bAbILogger(output_fpath=args.file_name, data_module=data_module))
+
+    elif args.dataset_name == "GSM":
+        from data_modules.GSM import GSMDataModule
+        from logger.GSM import GSMLogger
+
+        args.demonstration_indices = ast.literal_eval(args.demonstration_indices)
+
+        data_module = GSMDataModule(
+            model_name=args.model_name,
+            batch_size=args.batch_size,
+            data_directory=args.data_directory,
+            demonstration_indices=args.demonstration_indices,
+            num_demonstrations=args.num_demonstrations,
+            entity_augmentation=args.entity_augmentation,
+            prompt_augmentation=args.prompt_augmentation,
         )
 
-        datamodule.setup()
-        logger = GSMLogger("runs.json", datamodule)
+        if args.enable_wandb:
+            callbacks.append(GSMLogger(output_fpath=args.file_name, data_module=data_module))
 
-        trainer = pl.Trainer(max_epochs=1, callbacks=[logger], accelerator="auto", logger=False)
-        trainer.validate(model=model, datamodule=datamodule)
+    model = QuestionAnswerModel(data_module, args.max_new_tokens)
 
-        break
+    trainer = pl.Trainer(
+        max_epochs=1,
+        callbacks=callbacks,
+        accelerator="auto",
+        logger=False,
+        num_sanity_val_steps=2
+    )
 
+    trainer.validate(model=model, datamodule=data_module)
